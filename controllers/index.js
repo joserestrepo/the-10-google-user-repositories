747b6d698a43202e9bd6d5b43025a0d891ed62d1
joserestrepo <joserestrepo301@gmail.com>
2024-01-28 11:13:51 -0500

    The response was changed to return an object

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,4 @@
-'user strict'
+"user strict";
 
 const { getRepositoriesByUser } = require("./../services");
 
@@ -6,35 +6,18 @@ async function getRepositoriesByUserController(req, res) {
   try {
     const username = "google";
     const repositories = await getRepositoriesByUser(username);
-    const html = `
-      <!DOCTYPE html>
-      <html lang="en">
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Top 10 Repositorios de ${username} en GitHub</title>
-      </head>
-      <body>
-        <h1>Top 10 Repositorios de ${username} en GitHub</h1>
-        <ul>
-          ${repositories
-            .map(
-              (repositori) =>
-                `<li>${repositori.name} - ${repositori.stargazers_count} estrellas</li>`
-            )
-            .join("")}
-        </ul>
-      </body>
-      </html>
-    `;
-
-    res.send(html);
+    res.send(
+      repositories.map((repository) => ({
+        name: repository.name,
+        stargazersCount: repository.stargazers_count,
+      }))
+    );
   } catch (error) {
-    console.error('error when making the request:', error.message);
-    res.status(500).send('Internal error');
+    console.error("error when making the request:", error.message);
+    res.status(500).send("Internal error");
   }
 }
 
 module.exports = {
-    getRepositoriesByUserController
-}
+  getRepositoriesByUserController,
+};
